fe77bc76fbead508da088b9813fb85d1d05dfacb
olaemmadayo <olatoye.temidayo@yahoo.com>
2025-08-12 13:35:22 +0100

    about section is updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -15,7 +15,7 @@ export const AboutSection = () => {
           <span className="text-primary"> Me</span>
         </h2>
 
-        <div className="grid grid-cols-2 md:grid-cols-2 gap-12 items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6 ">
             <h3 className="text-2xl font-semibold">Passionate Web Developer & Tech Bro</h3>
             <p className="text-muted-foreground">
